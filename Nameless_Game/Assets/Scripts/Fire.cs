0c0ff49f7922775984d6403fd8f14ef16041e778
emousavi97 <emousavi97@gmail.com>
2017-09-03 12:03:18 +0430

    Fire
    Player can fire and shots are reflecting.

---
+++ Nameless_Game/Assets/Scripts/Fire.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+	float timer = 0f;
+	public float timebtwn = 0.3f;
+	public GameObject shot;
+	public GameObject shotspon;
+	float camraylength=100f;
+	int floorMask; 
+	public GameObject ganpivot;
+
+	void Awake(){
+		floorMask = LayerMask.GetMask ("Floor");
+	}
+	
+
+	void Update () {
+		timer += Time.deltaTime;
+		if (Input.GetButton ("Fire1") && timer >= timebtwn ) {
+			Instantiate (shot, shotspon.transform.position, shotspon.transform.rotation);
+			timer = 0f;
+		}
+		Turning ();
+	}
+
+	void Turning(){
+		
+		Ray CamRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit FloorHit;
+
+		if (Physics.Raycast (CamRay, out FloorHit, camraylength, floorMask)) {
+
+			Vector3 playerToMouse = FloorHit.point - ganpivot.transform.position;
+			playerToMouse.z = 0f;
+
+			ganpivot.transform.rotation = Quaternion.LookRotation (playerToMouse,transform.forward);
+
+		}
+	}
+}
---
+++ Nameless_Game/Assets/Scripts/ShotControler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotControler : MonoBehaviour {
+	Rigidbody rb;
+	public float speed=5f;
+	float timer=0f;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.velocity = transform.forward * speed;
+		timer += Time.deltaTime;
+	}
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.CompareTag ("Shot")) {
+			Destroy (collision.gameObject);
+			Destroy (gameObject);
+
+		} else if (collision.gameObject.CompareTag ("E")) {
+			
+			Vector3 normalE = new Vector3 (-1f, 0f, 0f);
+			Vector3 OutVec = Vector3.Reflect (transform.forward, normalE);
+			transform.rotation = Quaternion.LookRotation (OutVec);
+				
+		} else if (collision.gameObject.CompareTag ("W")) {
+					
+			Vector3 normalW = new Vector3 (1f, 0f, 0f);
+			Vector3 OutVec = Vector3.Reflect (transform.forward, normalW);
+			transform.rotation = Quaternion.LookRotation (OutVec);
+
+		} else if (collision.gameObject.CompareTag ("S")) {
+					
+			Vector3 normalS = new Vector3 (0f, 1f, 0f);
+			Vector3 OutVec = Vector3.Reflect (transform.forward, normalS);
+			transform.rotation = Quaternion.LookRotation (OutVec);
+
+		} else if (collision.gameObject.CompareTag ("N")) {
+					
+			Vector3 normalN = new Vector3 (0f, -1f, 0f);
+			Vector3 OutVec = Vector3.Reflect (transform.forward, normalN);
+			transform.rotation = Quaternion.LookRotation (OutVec);
+
+		} else if (collision.gameObject.CompareTag ("Player") && timer>0.2f) {
+			Destroy (gameObject);
+		}
+	}
+
+}
